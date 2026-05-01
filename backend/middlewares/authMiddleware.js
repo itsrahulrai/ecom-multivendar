6c6c988eb4e2eb24eb69caf890d8cdb3ae84dee0
Rahul Kumar <therahulray@gmail.com>
2026-05-02 00:09:27 +0530

    Middleware and routes manage part 1

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+const authMiddleware = async(req, res, next) => {
+  const { accessToken } = req.cookies;
+
+  if (!accessToken) {
+    return res.status(401).json({ error: "Please login first" });
+  }
+
+  try {
+    const decodedToken = await jwt.verify(accessToken, process.env.SECRET);
+
+    req.id = decodedToken.id;
+    req.role = decodedToken.role;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: "Invalid or expired token" });
+  }
+};
+
+export default authMiddleware;
